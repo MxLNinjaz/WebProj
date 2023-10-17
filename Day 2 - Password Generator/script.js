33eb8f7da6d1c090f54056770f5478ab89ee88c4
MxLNinjaz <joshfowler2007@gmail.com>
2023-10-17 17:28:09 +0100

    Updated

--- Day 2 - Password Generator/script.js
+++ Day 2 - Password Generator/script.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   var slider = document.getElementById("sliderRange");
   var output = document.getElementById("sliderNum");
 
-  // Display the default value
   output.textContent = slider.value;
 
-  // Update the displayed value as the slider is moved
   slider.addEventListener("input", function () {
     output.textContent = this.value;
   });
